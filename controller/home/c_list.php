62f91966cfd99d973258befae6ab2ea90f09b220
AmpapornDev <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>
2023-10-25 21:54:31 +0700

    first commit

---
+++ controller/home/c_list.php
@@ -0,0 +1,14 @@
+<?php
+
+$sql = "SELECT * FROM tb_book order by date_time DESC"; 
+$query = $mysqli->query($sql);
+if($query){
+    while($result = $query->fetch_assoc()){
+		$rows[]=$result;
+	}
+}else{
+    
+	$rows = 'null';
+}
+
+?>
